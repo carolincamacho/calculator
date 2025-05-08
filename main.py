0e377cdcac2e2ac2f9ce897ac4194aa58651acf9
carolincamacho <carolincamacho@javerianacali.edu.co>
2025-05-08 15:47:27 -0500

    tarea_calculator_may08

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ def home ():
 
 @app.route("/suma")
 def suma():
-    return render_template('suma.html')
+    num1=random.randint(1,1000000)
+    num2=random.randint(1,1000000)
+    if num1 is None or num2 is None:
+        return "faltan datos"
+    resultado=sumar(num1,num2)
+    return render_template('suma.html',resultado=resultado,num1=num1,num2=num2)
 
 @app.route("/resta")
 def resta():
@@ -20,8 +25,7 @@ def resta():
     if num1 is None or num2 is None:
         return "faltan datos"
     resultado=restar(num1,num2)
-    return render_template("resta.html",resultado=resultado,num1=num1, num2=num2)
-    #return f"<p><h2>La resta de los numeros {num1} y {num2} es: {restar(num1,num2)}</h2></p>"
+    return render_template("resta.html",resultado=resultado,num1=num1,num2=num2)
     
 @app.route("/divide")
 def divide():
@@ -29,8 +33,11 @@ def divide():
     num2=random.randint(1,1000000)
     if num1 is None or num2 is None:
         return "faltan datos"
-    return f"<p><h2>la division de los numeros {num1} y {num2} es: {dividir(num1,num2)}</h2></p>" ''' <p><h2><a href=/> Volver </a></h2></p> 
-            '''
+    ''' asi era el codigo antes de utilizar archivos html
+    return f"<p><h2>la division de los numeros {num1} y {num2} es: {dividir(num1,num2)}</h2></p>" ''' 
+    '''<p><h2><a href=/> Volver </a></h2></p> '''
+    resultado=dividir(num1,num2)
+    return render_template("divide.html",resultado=resultado,num1=num1,num2=num2)
 
 @app.route("/multiplica")
 def multiplica():
@@ -38,8 +45,7 @@ def multiplica():
     num2=random.randint(1,1000000)
     if num1 is None or num2 is None:
         return "faltan datos"
-    return f"<p><h2>la multiplicación de los numeros {num1} y {num2} es: {multiplicar(num1,num2)}</h2></p>" ''' <p><h2><a href=/> Volver </a></h2></p> 
-            '''
+    return f"<p><h1>Calculadora con codigo sin html</h1></p><p><h2>la multiplicación de los numeros {num1} y {num2} es: {multiplicar(num1,num2)}</h2></p>"''' <p><h2><a href=/> Volver </a></h2></p> '''
 
 if __name__ == "__main__":
     server = Server(app.wsgi_app)
